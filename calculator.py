636ec15a2786f14c51e2f3594d1858c34ab1c08b
David Ward <david.a.ward@duke.edu>
2019-09-09 12:22:58 -0400

    Wrote function to analyze HDL test result

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL < 60:
+        return "Borderline low"
+    else:
+        return "low"
+
+
 def interface():
     print("My calculator program")
     keep_running = True
